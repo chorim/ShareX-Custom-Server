6b431fbeafedcf731066d31a8055df7c4733172a
Yukihira Nanako <yuki@yuki.kr>
2019-12-31 14:53:27 +0900

    Replace handler

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,11 @@
 import "./libs/dotenv"
 import express from "express"
 import v1 from "./routes/v1"
+import { createHandler } from "./utils/createResponse"
 
 export const app = express()
 
-app.get('/', (req: express.Request, res: express.Response) =>
-  res.status(200).json({status:true, message: "ShareX-Custom-Server is now working."}))
+app.get('/', createHandler((req, res) =>
+  res.sendDefaultResponse({status: true, message: "ShareX-Custom-Server is now working."})
+))
 app.use('/v1', v1)
