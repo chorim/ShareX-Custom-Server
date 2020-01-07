8183f38cd7df9e1f334dbef33400de4d81afa269
Yukihira Nanako <yuki@yuki.kr>
2020-01-07 17:13:36 +0900

    Add uploader module

--- src/routes/v1/upload.ts
+++ src/routes/v1/upload.ts
@@ -1,14 +1,31 @@
 import { upload } from "../../controllers/v1/upload"
 import { Router } from "express"
 import { diskStorage } from "multer"
+import createDirectory from "../../utils/createDirectory"
+import multer from "multer"
+
+type Request = Express.Request & {
+  body: { filename: string }
+}
 
 const storage = diskStorage({
-  destination: (req, file, cb) => {
-
+  destination: async (req, file, cb) => {
+    const dir = process.env.STORAGE_DIR as string
+    await createDirectory(dir)
+    cb(null, dir)
+  },
+  filename: (req: Request, file, cb) => {
+    const filename = (!req.body.filename) ? file.originalname : req.body.filename
+    cb(null, filename)
   }
 })
 
+const uploader = multer({
+  storage, limits: {
+    fileSize: 1024 * 1024 * 1024 * 10 // 10 MiB
+  }
+})
 const router = Router()
-router.post('/upload', upload)
+router.route('/upload').post(uploader.array('sharex', 100), upload)
 
 export default router
